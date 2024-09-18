dbc0dd665dc8857fefbfe48d022defc5f239f24d
Alan <bb090224jp@gmail.com>
2024-09-18 12:19:55 +0800

    fixcart_0918

--- src/app/component/productdetail/productdetail.component.ts
+++ src/app/component/productdetail/productdetail.component.ts
@@ -71,111 +71,7 @@ ngOnInit() {
   // };
 
 
-  // products = [
-  //   {
-  //     id: 1,
-  //     image: 'assets/images/flower.png',
-  //     title: '美麗小花_讓您每日心情美麗',
-  //     price: 100,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 43,
-  //   },
-  //   {
-  //     id: 2,
-  //     image: 'assets/images/cactus2.png',
-  //     title: '帥氣仙人掌_讓您外表英氣逼人',
-  //     price: 200,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 23,
-  //   },
-  //   {
-  //     id: 3,
-  //     image: 'assets/images/whiteheels.png',
-  //     title: '優雅白高跟_讓您成為閃耀的星',
-  //     price: 1500,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 120,
-  //   },
-  //   {
-  //     id: 4,
-  //     image: 'assets/images/rocket.png',
-  //     title: '霸氣火箭_您是所有人的焦點',
-  //     price: 3000,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 5,
-  //   },
-  //   {
-  //     id: 5,
-  //     image: 'assets/images/perfume.png',
-  //     title: '清新香氛_為生活增添一抹甜美',
-  //     price: 500,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 56,
-
-  //   },
-  //   {
-  //     id: 6,
-  //     image: 'assets/images/sun.png',
-  //     title: '元氣朝陽_讓每一天都充滿動力',
-  //     price: 50,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 1000,
-  //   },
-  //   {
-  //     id: 7,
-  //     image: 'assets/images/notebook.png',
-  //     title: '輕巧筆記本_讓每個靈感都能隨手記下',
-  //     price: 35,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 121,
-  //   },
-  //   {
-  //     id: 8,
-  //     image: 'assets/images/blanket.png',
-  //     title: '溫暖毛毯_讓每個懶洋洋的午後都更舒服',
-  //     price: 700,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 34,
-
-  //   },
-
-  //   {
-  //     id: 9,
-  //     image: 'assets/images/earrings.png',
-  //     title: '時尚耳環_讓每個微笑都更閃亮',
-  //     price: 1200,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 230,
-  //   },
-  //   {
-  //     id: 10,
-  //     image: 'assets/images/choco.png',
-  //     title: '繽紛巧克力_每一口都是驚奇',
-  //     price: 80,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 233,
-  //   },
-  //   {
-  //     id: 11,
-  //     image: 'assets/images/headphone.png',
-  //     title: '質感耳機_戴上的那一刻世界彷彿靜止',
-  //     price: 5000,
-  //     productdetailUrl: 'productdetail',
-  //     productUrl: 'single-product.html',
-  //     selledquant: 7,
-  //   },
-
-  // ];
+  
 
   // images = [
   //   { src: './assets/images/flower.png', alt: 'Slide 1' },
@@ -233,38 +129,35 @@ navigateToDisplay() {
 
   //放入圖片
   //控制大圖小圖移動 使用swiper
-  public swiperConfigbig: SwiperOptions = {
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: { clickable: true },
-    navigation: true,
+  // public swiperConfigbig: SwiperOptions = {
+  //   effect: 'fade',
+  //   fadeEffect: {
+  //     crossFade: true,
+  //   },
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: { clickable: true },
+  //   navigation: true,
 
-  };
+  // };
 
-  public swiperConfig: SwiperOptions = {
-    direction: 'horizontal', // 水平滚动
-    slidesPerView: 5,        // 同时显示五张图片
-    spaceBetween: 10,        // 图片之间的间距
-    autoplay: {
-      delay: 5000,           // 自动切换的时间间隔
-      disableOnInteraction: false,
-    },
-    pagination: { clickable: true }, // 分页器
-    navigation: true,        // 上下文箭头
-    loop: true,              // 循环播放
-  };
+  // public swiperConfig: SwiperOptions = {
+  //   direction: 'horizontal', // 水平滚动
+  //   slidesPerView: 5,        // 同时显示五张图片
+  //   spaceBetween: 10,        // 图片之间的间距
+  //   autoplay: {
+  //     delay: 5000,           // 自动切换的时间间隔
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: { clickable: true }, // 分页器
+  //   navigation: true,        // 上下文箭头
+  //   loop: true,              // 循环播放
+  // };
 
 
   //加入購物車
-
- 
-
 
   increaseQuantity() {
     if (this.inputValue < 100) {
@@ -284,19 +177,30 @@ navigateToDisplay() {
 
 
 
-
+//立即購買
   goToCart() {
 
     // this.router.navigate(['/cart'], { queryParams: { id: this.product?.id } });
 
+    // let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    // cartItems.push(this.product);
+    // localStorage.setItem('cartItems', JSON.stringify(cartItems));  // 存储到 localStorage
+
     let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-    cartItems.push(this.product);
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));  // 存储到 localStorage
-
+    cartItems.push({
+      name: this.product.MallProductName,  // 商品名称
+      image: this.product.Pimage,          // 商品图片
+      price: this.product.GoldAmount,      // 商品金额
+      quantity: this.product.selledquant,  // 商品数量
+    })
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  
     this.router.navigate(['/cart']);
-  }
+  };
 
 
+
+  //加入購物車
 
   addToCart() {
 
@@ -318,15 +222,27 @@ navigateToDisplay() {
     this.isModalVisible = true;
 
     // 使用 localStorage 存储商品到购物车
-    let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-    cartItems.push(this.product);  // 将当前产品添加到购物车
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));  // 存储到 localStorage
+    // let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    // cartItems.push(this.product);  // 将当前产品添加到购物车
+    // localStorage.setItem('cartItems', JSON.stringify(cartItems));  // 存储到 localStorage
 
+
+    let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    cartItems.push({
+      name: this.product.MallProductName,  // 商品名稱
+      image: this.product.Pimage,          // 商品圖片
+      price: this.product.GoldAmount,      // 商品金额
+      quantity: this.product.selledquant,  // 商品数量
+    })
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
     // 3秒后关闭模态框并导航到购物车页面
     setTimeout(() => {
       this.isModalVisible = false;
       this.router.navigate(['/cart']);  // 导航到购物车页面
     }, 3000); // 3秒后关闭
+
+  
+    
 
   }
 
